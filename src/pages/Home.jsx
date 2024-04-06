822875601b288cd36310d2798cc5120311ba7933
s0zzang <dlth8888@gmail.com>
2024-04-06 10:20:24 +0900

    test. crud 연결(client-id: 00.sample)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,15 @@ import useCustomAxios from "@hooks/useCustomAxios.mjs";
 import { useQuery } from "@tanstack/react-query";
 
 function Home() {
+  // crud 테스트
+  const axios = useCustomAxios();
+  const { data, isLoading, error, refetch } = useQuery({
+    queryKey: ["products"],
+    queryFn: () => axios.get("/products", {}),
+    select: (response) => response.data,
+  });
+  console.log(data);
+
   return <div>Home</div>;
 }
 

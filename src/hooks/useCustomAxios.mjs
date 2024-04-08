56349e22266dc67ea7925ad220b75c4905a8c7c8
s0zzang <dlth8888@gmail.com>
2024-04-08 10:29:22 +0900

    test. 코드 삭제

--- src/hooks/useCustomAxios.mjs
+++ src/hooks/useCustomAxios.mjs
@@ -1,15 +1,15 @@
 import axios from "axios";
 
-const API_SERVER = import.meta.env.VITE_API_SERVER;
+const { VITE_API_SERVER, VITE_API_SERVER_RCP } = import.meta.env;
 
-function useCustomAxios() {
+function useCustomAxios(rcpApi = false) {
   const instance = axios.create({
-    baseURL: API_SERVER,
+    baseURL: rcpApi ? VITE_API_SERVER_RCP : VITE_API_SERVER,
     timeout: 1000 * 10,
     headers: {
       "content-type": "application/json",
       accept: "application/json",
-      "client-id": "00-sample",
+      // "client-id": "00-sample",
     },
   });
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,4 @@
-import useCustomAxios from "@hooks/useCustomAxios.mjs";
-import { useQuery } from "@tanstack/react-query";
-
 function Home() {
-  // crud 테스트
-  const axios = useCustomAxios();
-  const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ["products"],
-    queryFn: () => axios.get("/products", {}),
-    select: (response) => response.data,
-  });
-  console.log(data);
-
   return <>Home</>;
 }
 

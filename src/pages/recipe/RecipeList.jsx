f35074688d7cc601a140072cebf15be93eaa5446
s0zzang <dlth8888@gmail.com>
2024-04-08 23:14:55 +0900

    design. 레시피 리스트

--- src/pages/recipe/RecipeList.jsx
+++ src/pages/recipe/RecipeList.jsx
@@ -1,8 +1,10 @@
-import { Button, LinkButton } from "@components/Button/Button";
+import { Button } from "@components/Button/Button";
+import List from "@components/List/List";
 import Search from "@components/Search/Search";
 import Title from "@components/Title/Title";
 import useCustomAxios from "@hooks/useCustomAxios.mjs";
 import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router-dom";
 
 function RcpList() {
   // crud 테스트
@@ -12,11 +14,23 @@ function RcpList() {
     queryFn: () => axios.get("/1/100", {}),
     select: (response) => response.data.COOKRCP01.row,
   });
-  // console.log(data);
+
+  const recipeItem = data?.map((item) => (
+    <li key={item["RCP_SEQ"]}>
+      <Link to="">
+        <img src={item["ATT_FILE_NO_MAIN"]} alt={item["RCP_NM"]} />
+        <p>{item["RCP_NM"]}</p>
+        <Button>{item["RCP_PAT2"]}</Button>
+        <Button>{item["RCP_WAY2"]}</Button>
+      </Link>
+    </li>
+  ));
+
   return (
     <>
       <Title>해머거 레시피</Title>
-      <Search></Search>
+      <Search />
+      <List recipeItem={recipeItem} />
     </>
   );
 }

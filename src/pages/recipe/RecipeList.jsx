cbe2c8a92961a1abd6ddde1e1e0ae904d96e7ee1
s0zzang <dlth8888@gmail.com>
2024-04-11 00:11:14 +0900

    feat. 페이지네이션 - 현재 페이지 활성화, 페이지네이션 클릭시 해당 데이터 호출

--- src/pages/recipe/RecipeList.jsx
+++ src/pages/recipe/RecipeList.jsx
@@ -1,4 +1,5 @@
 import { Tag } from "@components/Button/Button";
+import Pagination from "@components/Pagination/Pagination";
 import List from "@components/Recipe/List/List";
 import Search from "@components/Search/Search";
 import Title from "@components/Title/Title";
@@ -8,6 +9,8 @@ import { Link } from "react-router-dom";
 
 function RcpList() {
   const axios = useCustomAxios(true);
+  const axiosTest = useCustomAxios();
+
   const [data, setData] = useState([]);
   const [count, setCount] = useState(0);
   const [keyword, setKeyword] = useState("");
@@ -15,11 +18,15 @@ function RcpList() {
   const fetchData = async (url) => {
     const { data } = await axios(url);
     setData(data.COOKRCP01.row);
-    setCount(data.COOKRCP01["total_count"]);
+    setCount(Number(data.COOKRCP01["total_count"]));
+    console.log(data);
+
+    // const res = await axiosTest.get("/products");
+    // console.log(res.data);
   };
 
   useEffect(() => {
-    fetchData("/1/100");
+    fetchData(`/1/${import.meta.env.VITE_PAGINATION_LIMIT}`);
   }, []);
 
   const recipeItem = data?.map((item) => (
@@ -38,6 +45,7 @@ function RcpList() {
       <Title>해머거 레시피</Title>
       <Search fetchData={fetchData} setKeyword={setKeyword} />
       <List recipeItem={recipeItem} count={count} keyword={keyword} />
+      <Pagination totalCount={count} fetchData={fetchData} />
     </>
   );
 }

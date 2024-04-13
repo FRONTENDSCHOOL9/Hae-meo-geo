f563b9036bf3b6b89c4018f160dfb2322dfe7075
s0zzang <dlth8888@gmail.com>
2024-04-13 21:15:18 +0900

    feat. uploadImage 함수

--- src/components/Recipe/Detail/Reply/Register.jsx
+++ src/components/Recipe/Detail/Reply/Register.jsx
@@ -1,15 +1,15 @@
+import useCustomAxios from "@hooks/useCustomAxios.mjs";
 import useUserStore from "@zustand/userStore.mjs";
-import styles from "./Register.module.css";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import useCustomAxios from "@hooks/useCustomAxios.mjs";
+import uploadImage from "@utils/uploadImage.mjs";
+import styles from "./Register.module.css";
 
 function ReplyRegister({ rcpName, rcpNum, setRepliesFn }) {
-  const { replyRegister } = styles;
+  const { replyRegister, formWr, name, rating, attachWr, preview } = styles;
   const { user } = useUserStore();
   const axios = useCustomAxios();
-
-  // console.log(user, replies);
 
   const {
     register,
@@ -24,18 +24,25 @@ function ReplyRegister({ rcpName, rcpNum, setRepliesFn }) {
         type: "qna",
         product_id: rcpNum,
         title: `${rcpName} 후기`,
-        // "image": "sample-bugatti.png",
         extra: {
           rating: formData.rating,
         },
       };
+
+      if (formData?.image.length) {
+        formData.extra.image = await uploadImage(formData);
+        delete formData?.image;
+      } else {
+        delete formData?.image;
+      }
+
       const { data } = await axios.post("/posts", formData);
       const resPost = await axios.get(
         `/posts?type=qna&custom={"product_id": ${rcpNum}}`
       );
       setRepliesFn(resPost.data);
     } catch (err) {
-      console.error(err.response?.data.message);
+      console.error(err);
     }
   };
 
@@ -43,65 +50,82 @@ function ReplyRegister({ rcpName, rcpNum, setRepliesFn }) {
     <div className={replyRegister}>
       {user ? (
         <form onSubmit={handleSubmit(onSubmit)}>
-          <img src={user.profile} alt={user.name} />
-          {user.name}
-          <div>
-            <input
-              type="radio"
-              name="rating"
-              value="1"
-              {...register("rating", {
-                required: "별점을 등록하세요",
-              })}
-            />
-            <input
-              type="radio"
-              name="rating"
-              value="2"
-              {...register("rating", {
-                required: "별점을 등록하세요",
-              })}
-            />
-            <input
-              type="radio"
-              name="rating"
-              value="3"
-              {...register("rating", {
-                required: "별점을 등록하세요",
-              })}
-            />
-            <input
-              type="radio"
-              name="rating"
-              value="4"
-              {...register("rating", {
-                required: "별점을 등록하세요",
-              })}
-            />
-            <input
-              type="radio"
-              name="rating"
-              value="5"
-              {...register("rating", {
-                required: "별점을 등록하세요",
-              })}
-            />
-            {errors && <div>{errors.rating?.message}</div>}
+          <div className={formWr}>
+            <img src={user.profile} alt={user.name} />
+            <div>
+              <p className={name}>{user.name}</p>
+              <div className={rating}>
+                <input
+                  type="radio"
+                  name="rating"
+                  value="1"
+                  {...register("rating", {
+                    required: "별점을 등록하세요",
+                  })}
+                />
+                <input
+                  type="radio"
+                  name="rating"
+                  value="2"
+                  {...register("rating", {
+                    required: "별점을 등록하세요",
+                  })}
+                />
+                <input
+                  type="radio"
+                  name="rating"
+                  value="3"
+                  {...register("rating", {
+                    required: "별점을 등록하세요",
+                  })}
+                />
+                <input
+                  type="radio"
+                  name="rating"
+                  value="4"
+                  {...register("rating", {
+                    required: "별점을 등록하세요",
+                  })}
+                />
+                <input
+                  type="radio"
+                  name="rating"
+                  value="5"
+                  {...register("rating", {
+                    required: "별점을 등록하세요",
+                  })}
+                />
+                {errors && <div>{errors.rating?.message}</div>}
+              </div>
+              <textarea
+                name="content"
+                id="content"
+                cols="30"
+                rows="10"
+                {...register("content", {
+                  required: "내용을 입력하세요.",
+                  minLength: {
+                    value: 5,
+                    message: "5자 이상 입력해주세요.",
+                  },
+                })}
+              ></textarea>
+              {errors && <div>{errors.content?.message}</div>}
+            </div>
+            <div className={attachWr}>
+              <label htmlFor="image" className={preview}>
+                <img src="" alt="" />
+                선택
+              </label>
+              <input
+                type="file"
+                accept="image/*"
+                id="image"
+                placeholder="이미지를 선택하세요"
+                {...register("image")}
+              />
+            </div>
           </div>
-          <textarea
-            name="content"
-            id="content"
-            cols="30"
-            rows="10"
-            {...register("content", {
-              required: "내용을 입력하세요.",
-              minLength: {
-                value: 5,
-                message: "5자 이상 입력해주세요.",
-              },
-            })}
-          ></textarea>
-          {errors && <div>{errors.content?.message}</div>}
           <button>등록하기</button>
         </form>
       ) : (

4fcf57d1d3b4a10401a43ac37dcadf92238de95c
s0zzang <dlth8888@gmail.com>
2024-04-18 21:04:15 +0900

    comment. 주석 삭제

--- src/pages/recipe/RecipeList.jsx
+++ src/pages/recipe/RecipeList.jsx
@@ -77,21 +77,3 @@ function RcpList() {
 }
 
 export default RcpList;
-
-// 총 카운트 변경 : 카테고리 클릭, 검색 -> 구분하는 변수 만들기
-// 페이지네이션 클릭시 총카운트를 저장된 숫자 사용하기
-// 상태를 나타내는 불린에 따라 총카운트 변수에 할당하기
-// 상태를 나타내는 불린 = useState
-
-// 버튼, 클릭 시 페이지네이션이 리렌더링 되도록 작업하기 (위의껄 해보면 될듯?)
-
-/*
-
-- 처음 통신이 됐을 때 data?.total_count 값 전달
-- isTotalCountUpdate가 false면 data?.total_count가 저장된 totalCountStorage 값 
-- isTotalCountUpdate가 true면 data?.total_count, totalCountStorage에 data?.total_count값 저장
-
-* 마지막 페이지에서 무조건 1/12 단위로 불러오면 다 불러와짐 ;; 아놔
-* 4페이지에서 무조건 다 불러와짐 
-
-*/

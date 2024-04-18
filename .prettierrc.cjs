617de82cb32a6bb46633bbd749fdbd7300e69d6f
s0zzang <dlth8888@gmail.com>
2024-04-18 19:49:15 +0900

    chore. prettier 설정 변경 : 화살표 함수 매개변수 괄호 always

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,8 +3,8 @@ module.exports = {
   semi: true,
   useTabs: false,
   tabWidth: 2,
-  trailingComma: 'all',
+  trailingComma: "all",
   printWidth: 80,
-  arrowParens: 'avoid',
-  endOfLine: 'auto',
+  arrowParens: "always",
+  endOfLine: "auto",
 };

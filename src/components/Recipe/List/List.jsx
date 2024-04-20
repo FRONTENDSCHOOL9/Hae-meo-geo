c0e98666f74d051b1241c924a82d126d8def56ea
s0zzang <dlth8888@gmail.com>
2024-04-20 22:52:39 +0900

    feat. 로딩 애니메이션 변경

--- src/components/Recipe/List/List.jsx
+++ src/components/Recipe/List/List.jsx
@@ -5,22 +5,15 @@ List.propTypes = {
   recipeItem: PropTypes.array.isRequired,
   totalCount: PropTypes.number.isRequired,
   keyword: PropTypes.string.isRequired,
-  isLoading: PropTypes.bool.isRequired,
 };
 
-function List({ recipeItem, totalCount, keyword, isLoading }) {
+function List({ recipeItem, totalCount, keyword }) {
   return (
     <div className={styles.rcpList}>
-      {isLoading ? (
-        <p>
-          <span>{keyword}</span> 레시피를 준비하고 있어요 =3
-        </p>
-      ) : (
-        <p>
-          총 <span>{totalCount}</span>개의 <span>{keyword}</span> 레시피가
-          준비되어있어요 :-)
-        </p>
-      )}
+      <p>
+        총 <span>{totalCount}</span>개의 <span>{keyword}</span> 레시피를
+        준비했어요 :-)
+      </p>
       <ul>{recipeItem}</ul>
     </div>
   );

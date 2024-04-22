12c16320ecb574817ad0f39adff6d11e96bf5410
s0zzang <dlth8888@gmail.com>
2024-04-22 15:15:44 +0900

    fix. 북마크 컴포넌트를 해머거 레시피에서만 노출되도록 변경

--- src/components/Recipe/Detail/Sidebar/Sidebar.jsx
+++ src/components/Recipe/Detail/Sidebar/Sidebar.jsx
@@ -2,13 +2,15 @@ import Bookmark from "@components/Recipe/Detail/Bookmark/Bookmark";
 import Share from "@components/Recipe/Detail/Share/Share";
 import styles from "./Sidebar.module.css";
 
-function Sidebar({ id, name, image }) {
+function Sidebar({ id, name, image, type = "haeRcp" }) {
   const { sidebar, bookmark, share } = styles;
   return (
     <ul className={sidebar}>
-      <li>
-        <Bookmark className={bookmark} id={id}></Bookmark>
-      </li>
+      {type === "haeRcp" && (
+        <li>
+          <Bookmark className={bookmark} id={id}></Bookmark>
+        </li>
+      )}
       <li>
         <Share className={share} name={name} image={image}></Share>
       </li>

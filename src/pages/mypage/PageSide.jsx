bba56cd9cb04b634e0404bf5912dd59dbe54f6ba
taehee1228 <hantaehee12@naver.com>
2024-04-22 09:21:30 +0900

    fix. 로그아웃

--- src/pages/mypage/PageSide.jsx
+++ src/pages/mypage/PageSide.jsx
@@ -3,7 +3,17 @@ import styles from "@pages/mypage/PageSide.module.css";
 import useUserStore from "@zustand/userStore.mjs";
 
 function PageSide() {
-  const { user } = useUserStore();
+  const { setUser, user } = useUserStore();
+
+  const handleLogout = () => {
+    const confirmLogout = window.confirm("로그아웃 하시겠습니까?");
+    if (confirmLogout) {
+      setUser(null);
+      localStorage.removeItem("user");
+    } else {
+      return;
+    }
+  };
 
   return (
     <div className={styles.pageside}>
@@ -12,11 +22,13 @@ function PageSide() {
         <br />
         {user && <p className={styles.user}>{user.name}님 환영합니다.</p>}
       </wrap>
+
       <LinkButton
         to={"/user/login"}
         color="secondary"
         size="max"
         filled="false"
+        onClick={handleLogout}
       >
         로그아웃
       </LinkButton>

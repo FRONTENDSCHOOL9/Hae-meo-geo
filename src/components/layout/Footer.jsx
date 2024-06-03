66d8c463f3fdb0e54750eb8d584b77900b4b2bf4
s0zzang <dlth8888@gmail.com>
2024-06-03 16:13:16 +0900

    design. 레시피 배너, 푸터 사이즈 조정

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,30 +1,32 @@
 import styles from "./Footer.module.css";
 
 function Footer() {
-  const { footer, info, strong, copyright } = styles;
+  const { footer, footerInner, info, strong, copyright } = styles;
   return (
     <footer className={footer}>
-      <ul className={info}>
-        <li>
-          <a href="#">이용약관</a>
-        </li>
-        <li>
-          <a href="#">
-            <strong>개인정보처리방침</strong>
-          </a>
-        </li>
-        <li>
-          <a href="#">FAQ</a>
-        </li>
-        <li>
-          <a href="#">공지사항</a>
-        </li>
-      </ul>
-      <ul className={info}>
-        <li className={strong}>해머거(주)</li>
-        <li>서울 종로구 종로3길 17, 광화문 D타워 D1동</li>
-      </ul>
-      <p className={copyright}>copyright 해머거(주). All Rights Reserved.</p>
+      <div className={footerInner}>
+        <ul className={info}>
+          <li>
+            <a href="#">이용약관</a>
+          </li>
+          <li>
+            <a href="#">
+              <strong>개인정보처리방침</strong>
+            </a>
+          </li>
+          <li>
+            <a href="#">FAQ</a>
+          </li>
+          <li>
+            <a href="#">공지사항</a>
+          </li>
+        </ul>
+        <ul className={info}>
+          <li className={strong}>해머거(주)</li>
+          <li>서울 종로구 종로3길 17, 광화문 D타워 D1동</li>
+        </ul>
+        <p className={copyright}>copyright 해머거(주). All Rights Reserved.</p>
+      </div>
     </footer>
   );
 }

4f0291c124eb8b6e7f49923dc2fb5d152a117c91
sozzi0 <gamjastore@naver.com>
2024-06-10 15:25:34 +0900

    docs. 인풋 읽기 전용으로 변경

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -1,87 +1,77 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import userStore from "@zustand/userStore.mjs";
 import useCustomAxios from "@hooks/useCustomAxios.mjs";
-import { Button, LinkButton } from "@components/Button/Button";
+import { LinkButton } from "@components/Button/Button";
 import styles from "./MyPage.module.css";
-import ReplyStyle from "../../components/Recipe/Detail/Reply/Reply.module.css";
 import MypageMenu from "@components/Mypage/MypageMenu";
+import { useNavigate } from "react-router-dom";
+import modalStore from "@zustand/modalStore.mjs";
 
 function MyPage() {
-  const [attachImg, setAttachImg] = useState();
-  const [userProfile, setUserPrifile] = useState();
   const { user, setUser } = userStore();
   const axios = useCustomAxios();
+  const navigate = useNavigate();
+  const { setModal } = modalStore();
   const isHttpUrl = /^(http|https):\/\//;
-  const [emailAvailability, setEmailAvailability] = useState(null);
-  const { mypage, myheader, myprofile, modify, profilewrapper, flexWr } =
-    styles;
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-  const { ref } = register("profileImage");
-  const file = useRef();
-  const email = watch("email");
-  const fetchData = async () => {
+  const { mypage, myheader, myprofile, modify } = styles;
+  const { handleSubmit } = useForm();
+
+  const [profile, setProfile] = useState();
+  const [birthday, setBirthday] = useState();
+
+  const fetchProfile = async () => {
     try {
-      const res = await axios.get(`/users/${user._id}/profileImage`);
-      setUserPrifile(res.data.item);
+      const res = await axios.get(`/users/${user._id}`);
+      setProfile(res.data.item);
+      console.log("Profile data:", res.data.item);
+    } catch (err) {
+      console.error(err.response?.data.message);
+    }
+  };
+
+  const fetchBirthday = async () => {
+    try {
+      const res = await axios.get(`/users/${user._id}`);
+      setBirthday(res.data.item.extra?.birthday);
+      console.log("Birthday data:", res.data.item.extra?.birthday);
     } catch (err) {
       console.error(err.response?.data.message);
     }
   };
 
   const handleLogout = () => {
-    const confirmLogout = window.confirm("로그아웃 하시겠습니까?");
-    if (confirmLogout) {
-      setUser(null);
-      localStorage.removeItem("user");
-
-      navigate("/user/login");
-    } else {
-    }
-  };
-
-  const checkEmailAvailability = async () => {
-    try {
-      const res = await axios.get(`/users/email?email=${email}`);
-      setEmailAvailability(res.data.ok && "사용 가능한 이메일입니다.");
-    } catch (err) {
-      setEmailAvailability(
-        err.response.data.ok === 0 ? "이미 사용 중인 이메일입니다." : "",
-      );
-    }
-  };
-  const handleCheckEmail = async () => {
-    email && (await checkEmailAvailability());
-  };
-
-  const handleAttachRemove = () => {
-    setAttachImg(null);
-    file.current.value = "";
+    setModal({
+      message: "로그아웃 되었습니다.",
+      event: () => {
+        setUser(null);
+        localStorage.removeItem("user");
+        navigate("/user/login");
+      },
+    });
   };
 
   useEffect(() => {
-    fetchData();
+    fetchProfile();
+    fetchBirthday();
   }, []);
 
   return (
     <div className={mypage}>
       <section className={myheader}>
         <div className={myprofile}>
-          <img
-            src={
-              isHttpUrl.test(userProfile?.profileImage)
-                ? userProfile?.profileImage
-                : `${import.meta.env.VITE_API_SERVER}/files/${import.meta.env.VITE_CLIENT_ID}/${userProfile?.profileImage}`
-            }
-            alt="내 프로필 이미지"
-          />
+          {profile && (
+            <img
+              src={
+                isHttpUrl.test(profile?.profileImage)
+                  ? profile?.profileImage
+                  : `${import.meta.env.VITE_API_SERVER}/files/${import.meta.env.VITE_CLIENT_ID}/${profile?.profileImage}`
+              }
+              alt="내 프로필 이미지"
+            />
+          )}
         </div>
-        {user && <p className={styles.user}>{user.name}님 환영합니다.</p>}
+        {profile && <p className={styles.user}>{profile.name}</p>}
 
         <div className={styles["logout-box"]}>
           <LinkButton
@@ -100,75 +90,12 @@ function MyPage() {
         <form onSubmit={handleSubmit(onsubmit)}>
           <fieldset>
             <label htmlFor="email">아이디(이메일)</label>
-            <div className={flexWr}>
-              <input
-                type="email"
-                id="email"
-                placeholder={user.email}
-                {...register("email", {
-                  required: "이메일을 입력하세요.",
-                  pattern: {
-                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                    message: "이메일 형식이 아닙니다.",
-                  },
-                })}
-              />
-              <Button
-                type="button"
-                onClick={handleCheckEmail}
-                color="primary"
-                size="large"
-              >
-                확인
-              </Button>
-            </div>
-            {errors.email && <p>{errors.email.message}</p>}
-            {emailAvailability && <p>{emailAvailability}</p>}
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="password">비밀번호</label>
-            <input
-              type="password"
-              id="password"
-              {...register("password", {
-                required: "비밀번호를 입력하세요.",
-              })}
-            />
-            {errors.password && <p>{errors.password.message}</p>}
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="confirmPassword">비밀번호 확인</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              {...register("confirmPassword", {
-                extra: "confirmPassword",
-                required: "비밀번호를 다시 입력하세요.",
-                validate: (value) =>
-                  value === watch("password") ||
-                  "비밀번호가 일치하지 않습니다.",
-              })}
-            />
-            {errors.confirmPassword && <p>{errors.confirmPassword.message}</p>}
+            <input type="email" id="email" placeholder={user.email} readOnly />
           </fieldset>
 
           <fieldset>
             <label htmlFor="name">닉네임</label>
-            <input
-              type="text"
-              id="name"
-              placeholder={user.name}
-              {...register("name", {
-                required: "닉네임을 입력하세요.",
-                minLength: {
-                  value: 2,
-                  message: "닉네임은 2글자 이상 입력하세요.",
-                },
-              })}
-            />
-            {errors.name && <p>{errors.name.message}</p>}
+            <input type="text" id="name" placeholder={profile?.name} readOnly />
           </fieldset>
 
           <fieldset>
@@ -176,58 +103,20 @@ function MyPage() {
             <input
               type="text"
               id="birthday"
-              placeholder="MM-DD"
-              {...register("birthday", {
-                extra: "birthday",
-                pattern: {
-                  value: /^(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/,
-                  message: "올바른 형식으로 입력하세요. (MM-DD)",
-                },
-              })}
+              placeholder={birthday || "MM-DD"}
+              readOnly
             />
-            {errors.birthday && <p>{errors.birthday.message}</p>}
-          </fieldset>
-
-          <fieldset className={profilewrapper}>
-            <label htmlFor="profileImage" className="profilelabel">
-              프로필
-            </label>
-            <div
-              className={` ${styles.profile} ${ReplyStyle.attachWr} ${styles.attachWr} ${
-                attachImg ? ReplyStyle.act : ""
-              }`}
-            >
-              <label htmlFor="profileImage" className="profilelabel">
-                <img src={attachImg} alt="" />
-                <span className="hidden">첨부파일 선택</span>
-              </label>
-              <button type="button" onClick={handleAttachRemove}>
-                <span className="hidden">첨부파일 삭제</span>
-              </button>
-              <input
-                type="file"
-                accept="image/*"
-                id="profileImage"
-                {...register("profileImage", {
-                  onChange: (e) => {
-                    const reader = new FileReader();
-                    reader.readAsDataURL(e.target.files[0]);
-                    reader.onloadend = () => {
-                      setAttachImg(reader.result);
-                    };
-                  },
-                })}
-                ref={(e) => {
-                  ref(e);
-                  file.current = e;
-                }}
-              />
-            </div>
           </fieldset>
           <hr />
-          <Button type="submit" color="primary" size="large" filled="false">
+          <LinkButton
+            to={"/mypage/editprofile"}
+            type="button"
+            color="primary"
+            size="large"
+            filled="false"
+          >
             수정하기
-          </Button>
+          </LinkButton>
         </form>
       </section>
     </div>
